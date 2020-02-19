e89e7b7d11282426fcb7a23c6e49562f3a155f3d
lucaryholt <luca@ryholt.net>
2020-02-19 01:49:38 +0100

    initial commit

---
+++ client.py
@@ -0,0 +1,82 @@
+import socket, sys
+
+def connectionProtocol(conn):
+    sendMessage("com-0 " + get_ip())
+    if correctAcceptProtocol(receiveData(conn)):
+        sendMessage("com-0 accept")
+        return True
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('255.255.255.255', 1))
+        IP = s.getsockname()[0]
+    finally:
+        s.close()
+    return IP
+
+def correctAcceptProtocol(text):
+    data = text.split(" ")
+    return data[0] == "com-0" and data[1] == "accept" and isIP(data[2])
+
+def isIP(text):
+    return len(text.split(".")) == 4
+
+def sendMessage(text):
+    client.send((text).encode())
+
+def receiveData(socket):
+    while True:
+        data = client.recv(4096).decode()
+        if debug:
+            print("raw: " + data) #debug line
+        return data
+
+def readMessage(text):
+    return text.split("=")[1]
+
+def sendChatMessage(text):
+    global seqnr
+    increaseSeqnr()
+    sendMessage("msg-" + str(seqnr) + "=" + text)
+
+def increaseSeqnr():
+    global seqnr
+    seqnr = seqnr + 1
+
+def extractSeqnr(text):
+    return (text.split("=")[0]).split("-")[1]
+
+def correctSeqnr(text):
+    global seqnr
+    if int(extractSeqnr(text)) - seqnr == 1:
+        return True
+
+#The code starts here
+debug = False
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == "-help":
+        print("Use -debug to see raw incoming messages")
+        exit()
+    if sys.argv[1] == "-debug":
+        debug = True
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('0.0.0.0', 5000))
+seqnr = -1;
+
+if connectionProtocol(client) == False:
+    exit()
+print("The connection is ready!")
+
+message = input()
+while message != 'Q':
+    sendChatMessage(message)
+    data = receiveData(client)
+    if correctSeqnr(data):
+        print(readMessage(data))
+        increaseSeqnr()
+    message = input()
+
+client.close()
---
+++ server.py
@@ -0,0 +1,100 @@
+import socket, sys
+
+def sendMessage(text, conn):
+    conn.send((text).encode())
+
+def correctProtocol(text, protocol):
+    return text.split(" ")[0] == protocol
+
+def connectProtocol(conn):
+    data = receiveData(conn)
+    if correctProtocol(data, "com-0") and isIP(data):
+        sendMessage(("com-0 accept " + get_ip()), conn)
+        data = receiveData(conn)
+        if correctProtocol(data, "com-0"):
+            return True
+        else:
+            return False
+    else:
+        return
+
+def isIP(text):
+    str = text.split(" ")[1]
+    return len(str.split(".")) == 4
+
+def receiveData(conn):
+    while True:
+        data = conn.recv(4096).decode()
+        global debug
+        if debug:
+            print("raw: " + data) #debug line
+        return data
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('255.255.255.255', 1))
+        IP = s.getsockname()[0]
+    finally:
+        s.close()
+    return IP
+
+def sendMessage(text, conn):
+    conn.send((text).encode())
+
+def readMessage(text):
+    return text.split("=")[1]
+
+def sendServerMessage(conn):
+    global seqnr
+    increaseSeqnr()
+    sendMessage(("res-" + str(seqnr) + "=I am server"), conn)
+
+def increaseSeqnr():
+    global seqnr
+    seqnr = seqnr + 1
+
+def extractSeqnr(text):
+    return (text.split("=")[0]).split("-")[1]
+
+def correctSeqnr(text):
+    global seqnr
+    if seqnr - int(extractSeqnr(text)) == 0:
+        return True
+    else:
+        return False
+
+def readMessage(text):
+    return text.split("=")[1]
+
+#The code starts here
+debug = False
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == "-help":
+        print("Use -debug to see raw incoming messages")
+        exit()
+    if sys.argv[1] == "-debug":
+        debug = True
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.bind(('0.0.0.0', 5000))
+serv.listen(5)
+seqnr = 0
+
+while True:
+    conn, addr = serv.accept()
+
+    if connectProtocol(conn):
+        while 1:
+            data = receiveData(conn)
+            if correctSeqnr(data):
+                print(readMessage(data))
+                sendServerMessage(conn)
+                increaseSeqnr()
+        conn.close()
+        print('Client disconnected')
+    else:
+        conn.close()
+        print("Not correct protocol. Closing connection.")
+        break
